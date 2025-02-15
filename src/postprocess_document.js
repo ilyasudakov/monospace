27068842a3bb5b80fcec384969f86fafde37f249
ilyasudakov <ilyasudakov.dev@gmail.com>
2025-02-15 22:23:14 +0300

    Updated overall design

---
+++ src/postprocess_document.js
@@ -0,0 +1,53 @@
+function replace_links() {
+  const all_links = document.querySelectorAll("a");
+
+  for (const link of all_links) {
+    const href = new URL(link.href);
+    const text = link.innerHTML;
+
+    if (href.hostname != window.location.hostname) {
+      link.setAttribute("target", "_blank");
+    }
+
+    link.innerHTML = `
+      <span>${text}</span>
+      <svg xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 32 32">
+          <path fill="currentColor" d="M5 5v22h22V5zm2 2h18v18H7zm6 3v2h5.563L9.28 21.281l1.438 1.438L20 13.437V19h2v-9z" />
+      </svg>
+    `;
+  }
+}
+
+function fix_relative_path(href) {
+  let new_url = new URL(href).pathname;
+  let prod_hostname = "..";
+
+  const map_folders = {
+    "/assets/": `${prod_hostname}/assets/`,
+  };
+
+  for (const path of Object.keys(map_folders)) {
+    new_url = new_url.replaceAll(path, map_folders[path]);
+  }
+
+  console.log(new_url);
+
+  return new_url;
+}
+
+function fix_images() {
+  const all_images = document.querySelectorAll("img");
+
+  for (const image of all_images) {
+    image.src = fix_relative_path(image.src, "img");
+    image.setAttribute("loading", "lazy");
+  }
+}
+
+(() => {
+  replace_links();
+  fix_images();
+})();
