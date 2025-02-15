d7a998971441b76e94d0618d5e3ebb62613c99a9
ilyasudakov <ilyasudakov.dev@gmail.com>
2025-02-15 21:44:20 +0300

    Move from Pandoc to simple HTML+CSS stack

--- generate_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ru
-pandoc -s -f markdown -t html5 -o index.html index.md -c styles.css --template=../src/template.html
-
-cd ../en
-pandoc -s -f markdown -t html5 -o index.html index.md -c styles.css --template=../src/template.html
-
-echo "Files generated"
